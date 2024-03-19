a7f263e7710381f64883836e8c960c23ca07c863
Amira Ezzeldin <aezz4007@gmail.com>
2024-03-19 23:24:37 +0200

    Create a grid of 16x16

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const createSquare = () => {
     const square = document.createElement('div');
     square.setAttribute('class', 'square');
-    square.style.width = '50px';
-    square.style.height = '50px';
+    square.style.width = '35px';
+    square.style.height = '35px';
     square.style.boxSizing = 'border-box';
     square.style.border = 'solid 3px black';
+    square.style.flex = '1 1 6.25%';
     return square;
 }
 
 const container = document.querySelector('#container');
-for (let i = 0; i <= 256; i++) {
+for (let i = 0; i < 256; i++) {
     let currentSquare = createSquare();
     container.appendChild(currentSquare);
 }
