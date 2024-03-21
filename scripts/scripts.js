a2346bfaabce228861652ecfd9151c942c7e28a1
Amira Ezzeldin <aezz4007@gmail.com>
2024-03-22 01:11:21 +0200

    Create new grid with size specified by user

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -6,7 +6,7 @@
 
 import { Helpers } from './helpers.js';
 
-
+// Create one square
 const createSquare = (num) => {
     const container = document.querySelector('#container');
     const square = Helpers.createNode('square', 'div', '', container, 'class');
@@ -14,6 +14,7 @@ const createSquare = (num) => {
     return square;
 }
 
+// Apply style to one square
 const styleSquare = (square, num) => {
     square.style.boxSizing = 'border-box';
     square.style.border = 'solid 1px white';
@@ -54,6 +55,7 @@ const squareHover = (square) => {
     });
 }
 
+// Create a new grid with a num number of squares wide
 const createGrid = (num) => {
     for (let i = 0; i < Helpers.power(num, 2); i++) {
         let square = createSquare(num);
@@ -62,11 +64,13 @@ const createGrid = (num) => {
     }
 }
 
+// An event listener to the button used to clear the grid colors
 document.querySelector('#clear-grid').addEventListener('click', () => {
     let squares = document.querySelectorAll('.square');
     squares.forEach(square => squareBackground(1, square, 255, 255, 255))
 });
 
+// An event listener to the button that shows/hides gridlines
 document.querySelector('#toggle-grid').addEventListener('click', () => {
     let squares = document.querySelectorAll('.square');
     squares.forEach(square => {
@@ -76,5 +80,18 @@ document.querySelector('#toggle-grid').addEventListener('click', () => {
     });
 });
 
+// Create a new grid with number of squares specified by user
+document.querySelector('#new-grid').addEventListener('click', () => {
+    let container = document.querySelector('#container');
+    let num = prompt('Select a number less than or equals 100');
+    if (num != null) {
+        while (isNaN(num) || num > 100) {
+            num = prompt ('Select a number less than or equals 100');
+        }
+    } 
+        Helpers.clearNode(container);
+        createGrid(num);
+});
 
+// Call the function to create a default grid 
 createGrid(16);
